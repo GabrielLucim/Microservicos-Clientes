8a43477a031eaf38570c2f3979752d707ec84c6c
GabrielLucim <20241PVAI10030005@estudantes.ifpr.edu.br>
2026-09-14 19:50:09 -0300

    feat: adiciona listagem de cliente

--- microservicos/clientes/server.js
+++ microservicos/clientes/server.js
@@ -19,6 +19,21 @@ app.get("/", async (req, res) => {
     }
 });
 
+app.get("/clientes", async (req, res) => {
+    try {
+        const resultado = await db.query(
+            "SELECT * FROM clientes ORDER BY id"
+        );
+
+        res.json(resultado.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            erro: "Erro ao buscar clientes"
+        });
+    }
+});
+
 async function criarTabela() {
     await db.query(`
         CREATE TABLE IF NOT EXISTS clientes (

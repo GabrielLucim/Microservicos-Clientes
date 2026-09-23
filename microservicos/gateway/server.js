047db5ee479b7b795a033898370eb172ec12e981
GabrielLucim <20241PVAI10030005@estudantes.ifpr.edu.br>
2026-09-23 20:08:23 -0300

    feat: registra clientes no gateway

--- microservicos/gateway/server.js
+++ microservicos/gateway/server.js
@@ -5,11 +5,11 @@ const app = express();
 
 app.use(express.json());
 
-const PRODUTOS_URL =
-    process.env.PRODUTOS_URL || "http://localhost:3001";
+const PRODUTOS_URL = process.env.PRODUTOS_URL || "http://localhost:3001";
 
-const PEDIDOS_URL =
-    process.env.PEDIDOS_URL || "http://localhost:3002";
+const PEDIDOS_URL = process.env.PEDIDOS_URL || "http://localhost:3002";
+
+const CLIENTES_URL = process.env.CLIENTES_URL || "http://localhost:3003";
 
 
 app.use("/produtos", async (req, res) => {
@@ -52,6 +52,28 @@ app.use("/pedidos", async (req, res) => {
 
         return res.status(503).json({
             erro: "Serviço de Pedidos indisponível"
+        });
+    }
+});
+
+app.use("/clientes", async (req, res) => {
+    try {
+        const resposta = await axios({
+            method: req.method,
+            url: `${CLIENTES_URL}${req.originalUrl}`,
+            data: req.body,
+            params: req.query,
+            timeout: 3000
+        });
+
+        res.status(resposta.status).json(resposta.data);
+    } catch (erro) {
+        if (erro.response) {
+            return res.status(erro.response.status).json(erro.response.data);
+        }
+
+        return res.status(503).json({
+            erro: "Serviço de Clientes indisponível"
         });
     }
 });
